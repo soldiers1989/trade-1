7bfcccbb2d3c8dc86ca9447ab0823b3e4efae7e5
polly <wangruibo@oak-inc.com>
2017-12-18 19:33:34 +0800

    add new files

--- cube/log.cpp
+++ cube/log.cpp
@@ -1,3 +1,192 @@
 #include "log.h"
+#include "fd.h"
+#include <stdarg.h>
+#include <iostream>
 BEGIN_CUBE_NAMESPACE
+void log::debug(const char* format, ...) {
+	//message buffer
+	const int MSGSZ = 1024;
+	char msg[MSGSZ] = { 0 };
+
+	//format message
+	va_list args;
+	va_start(args, format);
+	int sz = vsnprintf(msg, MSGSZ - 1, format, args);
+	va_end(args);
+
+	//check message
+	if (sz < 0)
+		return;
+
+	//print message
+	print(level::debug, msg);
+}
+
+void log::info(const char* format, ...) {
+	//message buffer
+	const int MSGSZ = 1024;
+	char msg[MSGSZ] = { 0 };
+
+	//format message
+	va_list args;
+	va_start(args, format);
+	int sz = vsnprintf(msg, MSGSZ - 1, format, args);
+	va_end(args);
+
+	//check message
+	if (sz < 0)
+		return;
+
+	//print message
+	print(level::info, msg);
+}
+
+void log::warn(const char* format, ...) {
+	//message buffer
+	const int MSGSZ = 1024;
+	char msg[MSGSZ] = { 0 };
+
+	//format message
+	va_list args;
+	va_start(args, format);
+	int sz = vsnprintf(msg, MSGSZ - 1, format, args);
+	va_end(args);
+
+	//check message
+	if (sz < 0)
+		return;
+
+	//print message
+	print(level::warn, msg);
+}
+
+void log::error(const char* format, ...) {
+	//message buffer
+	const int MSGSZ = 1024;
+	char msg[MSGSZ] = { 0 };
+
+	//format message
+	va_list args;
+	va_start(args, format);
+	int sz = vsnprintf(msg, MSGSZ - 1, format, args);
+	va_end(args);
+
+	//check message
+	if (sz < 0)
+		return;
+
+	//print message
+	print(level::error, msg);
+}
+
+void log::fatal(const char* format, ...) {
+	//message buffer
+	const int MSGSZ = 1024;
+	char msg[MSGSZ] = { 0 };
+
+	//format message
+	va_list args;
+	va_start(args, format);
+	int sz = vsnprintf(msg, MSGSZ - 1, format, args);
+	va_end(args);
+
+	//check message
+	if (sz < 0)
+		return;
+
+	//print message
+	print(level::fatal, msg);
+}
+
+void log::print(level lvl, const char *msg) {
+	std::lock_guard<std::mutex> lock(_mutex);
+
+	//check level
+	if (lvl < _level)
+		return;
+
+	//print message to output
+	if (_printer != 0)
+		_printer->print(msg);
+}
+
+void log::set(level lvl) {
+	std::lock_guard<std::mutex> lock(_mutex);
+	_level = lvl;
+}
+
+void log::set(out out, const char *dir = ".", const char *name = "log", cut ct = cut::none, uint fszlimit = -1) {
+	std::lock_guard<std::mutex> lock(_mutex);
+	//free old printer
+	if (_printer != 0) {
+		delete _printer;
+	}
+
+	//create new printer
+	switch (out) {
+	case cube::log::console:
+		_printer = new console_printer();
+		break;
+	case cube::log::file:
+		_printer = new file_printer(dir, name, ct, fszlimit);
+		break;
+	default:
+		_printer = 0;
+		break;
+	}
+}
+
+void console_printer::print(const char *msg) {
+	std::cout << msg;
+}
+
+file_printer::file_printer(const std::string &dir, const std::string &name, log::cut ct = log::cut::none, uint fszlimit = -1) : _dir(dir), _name(name), _cutopt(ct), _fszlimit(fszlimit), _num(-1) {
+	switch (ct) {
+	case cube::log::none:
+		open_normal_file();
+		break;
+	case cube::log::size:
+		open_sized_file();
+		break;
+	case cube::log::daily:
+		open_daily_file();
+		break;
+	default:
+		break;
+	}
+}
+
+file_printer::~file_printer() {
+	if (_file.is_open()) {
+		_file.flush();
+		_file.close();
+	}
+}
+
+void file_printer::print(const char *msg) {
+	//check if need to cut file
+	int sz = (int)strlen(msg);
+	check_and_cut(sz);
+
+	//write to file
+	if (_file.good()) {
+		_file.write(msg, sz);
+	}
+}
+
+void file_printer::check_and_cut(int msgsz) {
+
+}
+
+void file_printer::open_sized_file() {
+
+}
+
+void file_printer::open_daily_file() {
+
+}
+
+void file_printer::open_normal_file() {
+	_file.open(cube::path::make(_dir, _name+".log"), std::ios::out);
+}
 END_CUBE_NAMESPACE
--- cube/log.h
+++ cube/log.h
@@ -1,12 +1,115 @@
 #pragma once
 #include "cube.h"
+#include <mutex>
+#include <fstream>
 BEGIN_CUBE_NAMESPACE
+class printer;
+
+//log class
 class log {
 public:
+	//log output option
+	typedef enum out { console = 0, file = 1 } out;
+	//cut option for file output 
+	typedef enum cut { none = 0, size = 1, daily = 2 } cut;
+
+	//log level option
+	typedef enum level { debug = 0, info = 1, warn = 2, error = 3, fatal = 4 } level;
+
+public:
+	/*
+	*	log method with 5 level
+	*@param format: message format
+	*@return:
+	*	void
+	*/
 	static void debug(const char* format, ...);
 	static void info(const char* format, ...);
 	static void warn(const char* format, ...);
 	static void error(const char* format, ...);
 	static void fatal(const char* format, ...);
+
+public:
+	/*
+	*	set log output level option, less than specified level will filter out
+	*@param lvl: in, level output
+	*@return:
+	*	void
+	*/
+	static void set(level lvl);
+
+	/*
+	*	set log output printer, directory/name/cut method must be specified if output is file, file size limit 
+	*must be specified if output is file and cut method is size limit.
+	*@param out: in, output method, file or console
+	*@param dir: in, log file directory
+	*@param name: in, log file name
+	*@param ct: in, log file cut method, none, size or daily
+	*@param fszlimit: in, file size limit if cut method is size cutting
+	*@return:
+	*	void
+	*/
+	static void set(out out, const char *dir = 0, const char *name = 0, cut ct = cut::none, uint fszlimit = -1);
+
+private:
+	/*
+	*	print message to output
+	*/
+	static void print(level lvl , const char *msg);
+
+private:
+	//output printer for log
+	static printer *_printer;
+	
+	//mutex for log
+	static std::mutex _mutex;
+
+	//level option
+	static level _level;
+};
+
+//printer class
+class printer {
+public:
+	virtual void print(const char *msg) = 0;
+};
+
+//console printer class
+class console_printer : public printer {
+public:
+	void print(const char *msg);
+};
+
+//file printer class
+class file_printer : public printer {
+public:
+	file_printer(const std::string &dir, const std::string &name, log::cut ct = log::cut::none, uint fszlimit = -1);
+	virtual ~file_printer();
+
+	void print(const char *msg);
+
+private:
+	//cut file if need
+	void check_and_cut(int msgsz);
+
+	void open_sized_file();
+
+	void open_daily_file();
+
+	void open_normal_file();
+private:
+	//output file
+	std::ofstream _file;
+
+	//file directory
+	std::string _dir;
+	//file name prefix
+	std::string _name;
+	//cut option
+	log::cut _cutopt;
+	//file size limit for cutting file
+	uint _fszlimit;
+	//file number for sized cutting file
+	int _num;
 };
 END_CUBE_NAMESPACE
